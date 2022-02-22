2a90131f8947022e28eb2b374ed9cc5c398109f4
Supercolbat <supercolbat@gmail.com>
2022-02-22 18:54:43 +0000

    Updated stuff

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# NASM-BrainF-Compiler
+# NASM BrainF Compiler
 Compiles Brainf*** code into NASM assembly and links it to create an executable.
+
+`tree.bf` does not compile for some reason. looking into it.
+
+# Features
+faster than interpreted (i think). buggy.
+
+doesnt beat creating C code then compiling with `-O3`... *yet!*
 
 # Usage
 The file is called `main.py` rn but you can change it or smth. The target file path **MUST** come after `main.py` (or whatever you named it).
@@ -17,3 +24,9 @@ You can also pass `--detail` to `main.py` to add some comments to the assembly f
 ```sh
 python main.py path/to/code.bf --detail
 ```
+
+# Todo:
+Further speed optimizations:
+- Constant operations (+++\[>++<-\] will always put 6 in the next pointer).
+- FASM (Flat Assembly)
+  - I don't know if FASM is faster than NASM but it's worth a shot.
--- optmain.py
+++ optmain.py
@@ -1,4 +1,9 @@
 """
+My go at optimizing the assembly code.
+this time it seriously might not work on all machines.
+
+so optimizing means trying out FASM? okay then.
+
 Taking inspiration from Porth by Tsoding
 Latest (go check it out!): https://gitlab.com/tsoding/porth/
 I'm looking through: https://github.com/tsoding/porth/blob/a6ddbe8f65c3a155932ca34217ceccee6b88b931/porth.py
@@ -23,51 +28,27 @@ def main():
     with open(sys.argv[1]) as file:
         code = list(filter(lambda c: c in "+-.,><[]", file.read()))
 
-    program = generate_nasm_linux_x86_64(code)
+    base = sys.argv[1].split(".")[0]
+    program = generate_fasm_linux_x86_64(code)
 
-    base_filename = sys.argv[1].split(".")[0]
-    with open(base_filename + ".asm", "w+") as f:
+    with open(base + ".asm", "w+") as f:
         f.write(program)
-    print(f"nasm -f elf64 {base_filename}.asm")
-    subprocess.Popen(["nasm", "-f elf64", base_filename + ".asm"])
-    print(f"ld -o {base_filename} {base_filename}.o")
-    subprocess.Popen(["ld", "-o", base_filename, base_filename + ".o"])
+    print(f"fasm {base}.asm")
+    subprocess.Popen(["fasm", base + ".asm"])
     
     
 
-def generate_nasm_linux_x86_64(code: str) -> str:
+def generate_fasm_linux_x86_64(code: str) -> str:
     asm = """\
-%define MEMORY_SIZE 1024
-    
-BITS 64
-section .text"""
-    asm += """
-putch:
-    sub     rsp, 24
-    mov     edx, 1
-    mov     BYTE [rsp+12], dil
-    lea     rsi, [rsp+12]
-    mov     edi, 1
-    mov     rax, 1
-    syscall
-    add     rsp, 24
-    ret""" if "." in code else ""
-    asm += """
-readch:
-    sub     rsp, 24
-    mov     edx, 1
-    mov     edi, 1
-    lea     rsi, [rsp+15]
-    xor     rax, rax
-    syscall
-    movzx   eax, BYTE [rsp+15]
-    add     rsp, 24
-    ret""" if "," in code else ""
-    asm += """
+format ELF64 executable
+entry _start
 
-global _start
+segment readable writeable
+    memory     times MEMORY_SIZE db 0
+
+section '.text' readable executable
 _start:{}
-    xor     rax, rax""".format("\n    ;; initialize pointer" if "--detail" in sys.argv else "")
+    mov     rax, 0""".format("\n    ;; initialize pointer" if "--detail" in sys.argv else "")
 
     index = 0
     loop_stack = []
@@ -102,9 +83,12 @@ _start:{}
             
         elif char == ".":
             asm += """
-    mov     edi, DWORD [memory + rax]
+    mov     edx, 1
+    mov     esi, 48
+    mov     edi, 1
     push    rax
-    call    putch
+    mov     rax, 1
+    syscall
     pop     rax"""
             
         elif char == ",":
@@ -135,10 +119,33 @@ close_{label}:"""
     mov     rax, 60
     mov     rdi, 0
     syscall
-
-section .data
-    memory     times MEMORY_SIZE db 0
 """
+
+    # Functions
+    asm += """
+putch:
+    sub     rsp, 24
+    mov     edx, 1
+    mov     BYTE [rsp+12], dil
+    lea     rsi, [rsp+12]
+    mov     edi, 1
+    mov     rax, 1
+    syscall
+    add     rsp, 24
+ret""" if "." in code else ""
+
+    asm += """
+readch:
+    sub     rsp, 24
+    mov     edx, 1
+    mov     edi, 1
+    lea     rsi, [rsp+15]
+    mov     rax, 0
+    syscall
+    movzx   eax, BYTE [rsp+15]
+    add     rsp, 24
+ret""" if "," in code else ""
+
     return asm
 
 if __name__ == "__main__":
